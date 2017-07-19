70b6b972f55ae2f80e3db11be8acaba9e36720c8
莪是坏人 <516775625@qq.commkdir>
2017-07-19 10:40:11 +0800

    添加initFirstData，方便加载数据

--- app/src/main/java/mr_immortalz/com/lazefragment/SecondFragment.java
+++ app/src/main/java/mr_immortalz/com/lazefragment/SecondFragment.java
@@ -1,5 +1,10 @@
 package mr_immortalz.com.lazefragment;
 
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
 /**
  * Created by asus on 2016/3/26.
  */
@@ -13,10 +18,23 @@ public class SecondFragment extends BaseFragment {
 
     @Override
     protected void initView() {
+        TextView view = (TextView) findView(R.id.tv);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(),DetialActivity.class));
+            }
+        });
 
     }
 
     @Override
     protected void initData() {
+        Log.d("lazyFragment", "SecondFragment加载");
+    }
+
+    @Override
+    protected void initFirstData() {
+        Log.d("lazyFragment", "SecondFragment的第一次数据"+getActivity());
     }
 }
